cfcb6efecb3fb3558f51bcc1e62d7b48c046b05b
pujiragil <ragilsg164@gmail.com>
2022-12-02 21:17:28 +0700

    create filter button dropdown component (mobile device)

--- components/Filter/index.js
+++ components/Filter/index.js
@@ -1,7 +1,10 @@
 export default function Filter() {
   return (
-    <div>
-      
+    <div className="mx-3.5 mb-3">
+      <div className="relative border border-[#C4C4C4]">
+        <button className="text-xs p-3 w-full text-left">FILTER</button>
+        <img className="absolute top-1/2 right-3" src="/arrow-down.svg" alt="arrow-down" />
+      </div>
     </div>
   )
 }
--- pages/catalog.js
+++ pages/catalog.js
@@ -1,10 +1,12 @@
-import { MainCard, SingleCard } from "../components/Card"
+import { SingleCard } from "../components/Card"
+import Filter from "../components/Filter"
 import BaseLayout from "../components/Layout/baseLayout"
 
 export default function Catalog() {
   return (
     <BaseLayout title={"Mafesyen - Catalog Page"}>
       <SingleCard margin={"m-3.5"} bgLink={"/images/banner-2.png"} />
+      <Filter/>
     </BaseLayout>
   )
 }
