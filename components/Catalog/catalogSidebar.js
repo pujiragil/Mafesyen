9cccc748d86957cf66a442c573d1638ca4e2a8d4
pujiragil <ragilsg164@gmail.com>
2023-01-29 22:31:42 +0700

    comp: create checkboxItem component

--- components/Catalog/catalogSidebar.js
+++ components/Catalog/catalogSidebar.js
@@ -3,31 +3,21 @@ export function CatalogSidebar() {
     <div className="h-full space-y-8 border-r border-[#C4C4C4]">
       <h3 className="text-2xl font-normal">Shop Some Wear:</h3>
       <div className="space-y-5">
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">best sellers</label>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">new arivals</label>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">top woman</label>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">collection: summer</label>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">collection: spring</label>
-        </div>
-        <div className="flex items-center gap-x-2">
-          <input type={"checkbox"} />
-          <label className="font-roboto text-sm text-[#3F3F3F] uppercase">trending</label>
-        </div>
+        <CheckboxItem>best sellers</CheckboxItem>
+        <CheckboxItem>new arivals</CheckboxItem>
+        <CheckboxItem>collection: summer</CheckboxItem>
+        <CheckboxItem>collection: spring</CheckboxItem>
+        <CheckboxItem>trending</CheckboxItem>
       </div>
     </div>
+  )
+}
+
+function CheckboxItem({ children }) {
+  return (
+    <label className="flex items-center gap-x-2 font-roboto text-sm text-[#3F3F3F] uppercase">
+      <input type="checkbox" className="peer" />
+      <p className="peer-checked:text-black">{children}</p>
+    </label>
   )
 }
