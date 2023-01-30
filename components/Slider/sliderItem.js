a27a0876ecac49a05244f80bcd162194375558a2
pujiragil <ragilsg164@gmail.com>
2023-01-30 20:50:51 +0700

    comp: create slider item component

---
+++ components/Slider/sliderItem.js
@@ -0,0 +1,20 @@
+import { Button } from "../Global";
+
+export default function SliderItem() {
+  return (
+    <div className="min-h-[400px] gap-y-5 pt-8 lg:px-8 pb-20 bg-[url('/images/swiper-bg.png')] bg-no-repeat bg-cover flex flex-col lg:flex-row justify-center items-center text-center">
+      <div className="lg:order-2 w-full flex justify-center items-center">
+        <img
+          className="h-[200px] w-[205px] md:h-[300px] md:w-[305px] lg:w-[450px] lg:h-auto object-cover"
+          src="/images/swiper-1.png"
+        />
+      </div>
+      <div className="lg:order-1 gap-y-3.5 flex flex-col items-center text-center">
+        <h1 className="w-8/12 text-3xl md:text-4xl lg:text-6xl font-semibold uppercase">
+          SUMMER SALE GEt 30% OFF On all dress.
+        </h1>
+        <Button mainButton={true}>shop now</Button>
+      </div>
+    </div>
+  )
+}
