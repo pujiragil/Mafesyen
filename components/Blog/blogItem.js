adfe5e5b89073f43124fbd9081f299dc77ab5c69
pujiragil <ragilsg164@gmail.com>
2023-06-08 23:12:12 +0700

    added new props to BlogItem component

--- components/Blog/blogItem.js
+++ components/Blog/blogItem.js
@@ -1,24 +1,20 @@
 import { Highlight } from "../Global";
 
-export default function BlogItem() {
+export default function BlogItem({ highlight, title, description, date }) {
   return (
     <article className="h-56 md:h-72 lg:h-[350px]  p-3 flex flex-col justify-between bg-white font-oswald">
       <div>
-        <Highlight sizes="lg" text="TOP WOMAN" />
-        <h4 className="text-sm md:text-base lg:text-lg mt-1 mb-3.5">
-          Exactly What To Wear To Every Type Of Wedding This Summer
-        </h4>
+        <Highlight sizes="lg" text={highlight} />
+        <h4 className="text-sm md:text-base lg:text-lg mt-1 mb-3.5">{title}</h4>
         <p className="font-roboto text-[10px] md:text-xs lg:text-sm">
-          A large part of the business here is building up the &quot;experience&quot;.
-          Double-page spreads in magazines. Ads on TV. Product placement and
-          sponsorship in golf tournaments, tennis matches and sailing
-          competitions. All of this builds up your perception of the brand - and
-          additionally, it costs money.
+          {description}
         </p>
       </div>
       <div className="border-t-2 border-black py-1">
-        <p className="font-roboto text-[10px] md:text-xs lg:text-sm">30 October 2022 by Ragil</p>
+        <p className="font-roboto text-[10px] md:text-xs lg:text-sm">
+          {date} by Ragil
+        </p>
       </div>
     </article>
-  )
+  );
 }
