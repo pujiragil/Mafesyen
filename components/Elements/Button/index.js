8707a733fc47e0976558668746b2b34597ad3a3f
pujiragil <ragilsg164@gmail.com>
2023-06-10 23:31:08 +0700

    implemented button style cva to component

--- components/Elements/Button/index.js
+++ components/Elements/Button/index.js
@@ -1,33 +1,51 @@
 import { cva } from "class-variance-authority";
+import { cn } from "../../../utils/utils";
 
-const button = cva(["font-oswald", "font-medium", "uppercase"], {
-  variants: {
-    intent: {
-      primary: "border-[1.5px] text-black",
+const button = cva(
+  ["font-oswald", "font-medium", "uppercase", "py-2", "md:py-3.5"],
+  {
+    variants: {
+      intent: {
+        primary: "border-[1.5px] border-black text-black md:border-2",
+      },
+      size: {
+        base: "text-[10px] md:text-lg",
+        lg: "text-sm md:text-lg",
+      },
+      width: {
+        base: "w-[85px] h-8 md:w-44 md:h-14",
+        full: "w-full",
+      },
     },
-    size: {
-      base: "text-[10px] md:text-lg",
-      lg: "text-sm md:text-lg",
-    },
-    width: {
-      base: "w-[85px] h-8 md:w-44 md:h-14",
-      full: "w-full",
-    },
-  },
-  compoundVariants: [
-    {
-      intent: "primary",
-      size: "lg",
-      className: "w-[115px] h-10",
-    },
-    {
+    compoundVariants: [
+      {
+        intent: "primary",
+        size: "lg",
+        className: "w-[115px] h-10 border-2",
+      },
+      {
+        intent: "primary",
+        size: "base",
+        className: "font-semibold",
+      },
+    ],
+    defaultVariants: {
       intent: "primary",
       size: "base",
-      className: "font-semibold",
+      width: "base",
     },
-  ],
-  defaultVariants: {
-    intent: "primary",
-    size: "base",
-  },
-});
+  }
+);
+
+const Button = ({ intent, size, width, className, ...props }) => {
+  return (
+    <button
+      className={cn(button({ intent, size, width, className }))}
+      {...props}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
