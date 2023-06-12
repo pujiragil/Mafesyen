d0731497f8370a0f684d472676b837333876ff3f
pujiragil <ragilsg164@gmail.com>
2023-06-12 22:12:30 +0700

    created custom svg component

---
+++ components/Assets/arrow.js
@@ -0,0 +1,33 @@
+export const ArrowNavigationRight = ({ className }) => (
+  <svg
+    className={className}
+    width="9"
+    height="14"
+    viewBox="0 0 9 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.982422 13.24L6.78052 7.4419L0.982423 1.6438"
+      stroke="white"
+      strokeWidth="2"
+    />
+  </svg>
+);
+
+export const ArrowNavigationLeft = ({ className }) => (
+  <svg
+    className={className}
+    width="9"
+    height="14"
+    viewBox="0 0 9 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.01758 0.76001L2.21948 6.5581L8.01758 12.3562"
+      stroke="white"
+      strokeWidth="2"
+    />
+  </svg>
+);
