df453df3608bbaf7d96e61c785be55f4638bd130
pujiragil <ragilsg164@gmail.com>
2023-06-12 21:56:16 +0700

    change swiper image size

--- components/Slider/sliderItem.js
+++ components/Slider/sliderItem.js
@@ -4,16 +4,16 @@ import Button from "../Elements/Button";
 
 const SlideItem = memo(() => {
   return (
-    <div className="flex flex-col gap-5 px-3.5 pt-8 pb-20 md:grid md:min-h-[500px] md:grid-cols-2 md:place-items-center md:px-5 lg:gap-0 lg:px-10">
-      <div className="flex justify-center md:order-2">
+    <div className="flex flex-col gap-5 px-3.5 pt-8 pb-20 md:grid md:min-h-[500px] md:grid-cols-2 md:place-items-center md:px-8 lg:px-20">
+      <div className="flex justify-center md:order-2 md:justify-end">
         <img
           className="h-auto w-3/4 object-center md:w-full md:w-full lg:w-4/5"
           src="/images/swiper-1.png"
           alt="swiper-model"
         />
       </div>
-      <div className="flex flex-col items-center gap-3.5 text-center md:order-1 md:items-start md:text-left">
-        <h1 className="w-9/12 font-oswald text-[2rem] font-semibold uppercase leading-9 text-black md:w-full md:text-[3.5rem] md:leading-none lg:text-[4.5rem]">
+      <div className="flex flex-col items-center gap-3.5 text-center md:order-1 md:items-start md:gap-8 md:text-left lg:gap-10">
+        <h1 className="w-9/12 font-oswald text-[2rem] font-semibold uppercase leading-9 text-black md:w-full md:text-[3.5rem] md:leading-none lg:w-10/12 lg:text-[4.5rem]">
           summer sale get 30% off on all dress.
         </h1>
         <Button intent="primary" size="lg" width="base">
