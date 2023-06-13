29747ad1fa19be03f933e901531e2a8c1eae1861
pujiragil <ragilsg164@gmail.com>
2023-06-13 22:13:33 +0700

    update dresses data

--- utils/dress.js
+++ utils/dress.js
@@ -1,1 +1,42 @@
-export const dresses = ["/images/dress-1.png", "/images/dress-2.png", "/images/dress-3.png", "/images/dress-4.png", "/images/dress-5.png", "/images/dress-6.png", "/images/dress-7.png", "/images/dress-8.png"]
+export const dresses = [
+  {
+    id: 1,
+    alt: "dress",
+    link: "/images/dress-1.webp",
+  },
+  {
+    id: 2,
+    alt: "dress",
+    link: "/images/dress-2.webp",
+  },
+  {
+    id: 3,
+    alt: "dress",
+    link: "/images/dress-3.webp",
+  },
+  {
+    id: 4,
+    alt: "dress",
+    link: "/images/dress-4.webp",
+  },
+  {
+    id: 5,
+    alt: "dress",
+    link: "/images/dress-5.webp",
+  },
+  {
+    id: 6,
+    alt: "dress",
+    link: "/images/dress-6.webp",
+  },
+  {
+    id: 7,
+    alt: "dress",
+    link: "/images/dress-7.webp",
+  },
+  {
+    id: 8,
+    alt: "dress",
+    link: "/images/dress-8.webp",
+  },
+];
