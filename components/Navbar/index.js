95d87a886352238ca61d8402ef7f463592959959
pujiragil <ragilsg164@gmail.com>
2023-06-13 20:28:22 +0700

    created nav asset svg component

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,36 +3,32 @@ import { useState } from "react";
 import { navLinks } from "../../utils/links";
 import { Head } from "../Global";
 import NavDropdown from "./navDropdown";
+import { HamburgerIcon, SearchIcon } from "../Assets/navbar";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-black text-white lg:mb-8 font-oswald">
-      <div className="container mx-auto relative">
+    <nav className="bg-black font-oswald text-white lg:mb-8">
+      <div className="container relative mx-auto">
         <div className="px-3.5">
-          <div className="h-14 flex items-center md:justify-between">
+          <div className="flex h-14 items-center md:justify-between">
             {/* NOTE hamburger */}
             <div className="w-1/4 md:hidden">
-              <img
-                onClick={() => setIsOpen(true)}
-                className="cursor-pointer"
-                src="/hamburger.svg"
-                alt="hamburger"
-              />
+              <HamburgerIcon onClick={() => setIsOpen(true)} />
             </div>
 
-            <div className="w-2/4 md:w-auto lg:w-3/12 flex justify-center lg:justify-start">
+            <div className="flex w-2/4 justify-center md:w-auto lg:w-3/12 lg:justify-start">
               <Link href={"/"}>
                 <Head />
               </Link>
             </div>
 
-            <div className="w-1/4 flex gap-x-5 items-center justify-end md:hidden">
-              <img src="/search.svg" alt="search-icon" />
+            <div className="flex w-1/4 items-center justify-end gap-x-5 md:hidden">
+              <SearchIcon />
               <span className="relative">
                 <img src="/cart.svg" alt="cart-icon" />
-                <span className="bg-[#EB5757] px-1 absolute -top-1.5 -right-1.5 text-[10px]">
+                <span className="absolute -top-1.5 -right-1.5 bg-[#EB5757] px-1 text-[10px]">
                   2
                 </span>
               </span>
@@ -53,14 +49,14 @@ export default function Navbar() {
 function NavLinks() {
   return (
     <>
-      <ul className="hidden md:flex lg:w-6/12 lg:justify-center gap-x-4 items-center uppercase">
+      <ul className="hidden items-center gap-x-4 uppercase md:flex lg:w-6/12 lg:justify-center">
         {navLinks.map((link) => (
           <li key={link.to}>
             <LinkItem to={link.to} name={link.name} />
           </li>
         ))}
       </ul>
-      <div className="hidden md:flex lg:w-3/12 lg:justify-end gap-x-4 items-center uppercase">
+      <div className="hidden items-center gap-x-4 uppercase md:flex lg:w-3/12 lg:justify-end">
         <LinkItem to="" name="sign in" />
         <LinkItem to="" name="create an account" />
       </div>
