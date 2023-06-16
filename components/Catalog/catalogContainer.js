8470f1689cfdb06e01d58f006f3969fdf0c5985b
pujiragil <ragilsg164@gmail.com>
2023-06-16 22:39:51 +0700

    try dynamic import

--- components/Catalog/catalogContainer.js
+++ components/Catalog/catalogContainer.js
@@ -1,13 +1,43 @@
+import dynamic from "next/dynamic";
 import { dresses } from "../../utils/dress";
 import { DetailButton } from "../Global";
 import { CatalogSidebar } from "./catalogSidebar";
-import ProductCard from "../Card/productCard";
+import { ProductCard } from "../organism/card/productCard";
+
+const ProductImage = dynamic(
+  () => import("../organism/card/productCard").then((mod) => mod.ProductImage),
+  {
+    ssr: false,
+    loading: () => (
+      <p className="w-full bg-red-500 text-black">Loading Image...</p>
+    ),
+  }
+);
+
+const ProductInfo = dynamic(
+  () => import("../organism/card/productCard").then((mod) => mod.ProductInfo),
+  {
+    ssr: false,
+    loading: () => (
+      <p className="w-full bg-blue-500 text-black">Loading Info...</p>
+    ),
+  }
+);
+
+const ProductPrice = dynamic(
+  () => import("../organism/card/productCard").then((mod) => mod.ProductPrice),
+  {
+    ssr: false,
+    loading: () => (
+      <p className="w-full bg-green-500 text-black">Loading Price...</p>
+    ),
+  }
+);
 
 export default function Catalog() {
   return (
     <div className="mx-3.5 mb-3.5 gap-x-8 font-oswald lg:mb-8 lg:flex">
       <h3 className="mb-4 text-lg md:text-xl lg:hidden">Shop Some Wear:</h3>
-
       <div className="hidden w-3/12 lg:block">
         <CatalogSidebar />
       </div>
@@ -18,9 +48,9 @@ export default function Catalog() {
             <ProductCard
               key={dress.id}
               data={dress}
-              image={<ProductCard.Image />}
-              info={<ProductCard.Info />}
-              price={<ProductCard.Price />}
+              image={<ProductImage />}
+              info={<ProductInfo />}
+              price={<ProductPrice />}
             />
           ))}
         </div>
