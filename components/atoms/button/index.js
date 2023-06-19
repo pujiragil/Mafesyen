fd1b5338f2a9c57966449bec4037d09bdd9e25ed
pujiragil <ragilsg164@gmail.com>
2023-06-19 11:58:41 +0700

    created regular Button component

---
+++ components/atoms/button/index.js
@@ -0,0 +1,37 @@
+import { cva } from "class-variance-authority";
+import { cn } from "@/utils/utils";
+
+const button = cva("py-2.5 font-oswald text-sm font-medium uppercase", {
+  variants: {
+    variant: {
+      primary: "bg-black text-white active:bg-[#EB5757]",
+      secondary:
+        "border-[#C4C4C4] bg-white text-[#3F3F3F] active:border-[#828282]",
+      more: " border-[#C4C4C4] bg-[#F0F2F2] text-[#828282] active:border-[#828282]",
+    },
+    width: {
+      full: "w-full",
+      medium: "w-full md:w-36",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: ["secondary", "more"],
+      className: "border-2",
+    },
+  ],
+  defaultVariants: {
+    variant: "primary",
+    width: "full",
+  },
+});
+
+const Button = ({ variant, width, className, children, ...props }) => {
+  return (
+    <button className={cn(button({ variant, width, className }))} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- components/atoms/index.js
+++ components/atoms/index.js
@@ -1,5 +1,6 @@
 export { default as BigHead } from "./head/bigHead";
 export { default as ButtonLink } from "./button/buttonLink";
+export { default as Button } from "./button";
 export { default as BrandImage } from "./brandImage";
 export { default as Category } from "./category";
 export { default as Text } from "./text";
