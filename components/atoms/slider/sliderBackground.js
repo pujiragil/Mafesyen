c65a5371395f07749caad5af36c7956313631e1a
pujiragil <ragilsg164@gmail.com>
2023-06-19 22:47:37 +0700

    created atom sliderBackground component

---
+++ components/atoms/slider/sliderBackground.js
@@ -0,0 +1,17 @@
+import { memo } from "react";
+import Image from "next/image";
+
+const SlideBackground = () => {
+  return (
+    <Image
+      width={1410}
+      height={601}
+      priority={true}
+      className="absolute top-0 left-0 z-0 h-[110%]"
+      src="/images/swiper-bg.webp"
+      alt="swiper-background"
+    />
+  );
+};
+
+export default memo(SlideBackground);
