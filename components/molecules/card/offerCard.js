bc0057ebb6a3b7221c757873305313d0498e27c8
pujiragil <ragilsg164@gmail.com>
2023-06-21 13:17:47 +0700

    applied content position offerCard

--- components/molecules/card/offerCard.js
+++ components/molecules/card/offerCard.js
@@ -3,11 +3,12 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/utils/utils";
 import { BigHead, ButtonLink, Text } from "@/components/atoms";
 
-const card = cva("relative z-10 grid w-full grid-cols-2 px-3.5 py-7", {
+const card = cva("relative z-10 grid w-full grid-cols-2 p-7", {
   variants: {
     variant: {
-      primary: "h-72 place-items-end md:row-span-2 md:h-full md:p-7",
-      secondary: "h-36 place-items-center md:h-full",
+      primary:
+        "h-72 content-end md:row-span-2 md:h-full md:place-items-center md:content-center",
+      secondary: "h-36 place-content-between content-center md:h-full",
     },
   },
 });
@@ -26,7 +27,7 @@ const OfferCard = ({ variant, order, className, data }) => {
           <BigHead type="h2" variant={headVariant}>
             {data.title}
           </BigHead>
-          <Text family="roboto" size="xs/lg">
+          <Text family="roboto" size="xs/lg" className="md:leading-snug">
             {data.description}
           </Text>
         </div>
@@ -39,7 +40,12 @@ const OfferCard = ({ variant, order, className, data }) => {
         height={434}
         src={data.imageBg}
         alt="offer-card"
-        className="absolute top-0 left-0 -z-10 h-full w-full object-cover object-top"
+        className={cn(
+          "absolute top-0 left-0 -z-10",
+          "h-full w-full",
+          "object-cover",
+          variant === "primary" ? "object-right-top" : "object-top"
+        )}
       />
     </div>
   );
--- components/molecules/index.js
+++ components/molecules/index.js
@@ -2,3 +2,4 @@ export * from "./card/productCard";
 export { default as Brand } from "./brand";
 export { default as SliderItem } from "./slider/sliderItem";
 export { default as SliderWrapper } from "./slider/sliderWrapper";
+export { default as OfferCard } from "./card/offerCard";
