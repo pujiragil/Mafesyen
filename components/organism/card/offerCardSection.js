dc78ba613dd1a70216646728bf5f241724091ee8
pujiragil <ragilsg164@gmail.com>
2023-06-21 13:21:06 +0700

    created organism offerCardSection component

---
+++ components/organism/card/offerCardSection.js
@@ -0,0 +1,43 @@
+import { OfferCard } from "@/components/molecules";
+
+const OfferCardSection = () => {
+  return (
+    <div className="h-full space-y-3.5 p-3.5 md:grid md:h-[500px] md:grid-cols-2 md:grid-rows-2 md:gap-3.5 md:space-y-0 lg:h-[560px] lg:gap-8">
+      <OfferCard
+        variant="secondary"
+        order="second"
+        data={{
+          title: "choose your look",
+          description: "See our clothing collections",
+          linkTo: "/catalog",
+          button: "see offers",
+          imageBg: "/images/card-1.webp",
+        }}
+      />
+      <OfferCard
+        variant="primary"
+        order="first"
+        data={{
+          title: "up to 40% off",
+          description: "Special offers and great deals",
+          linkTo: "/catalog",
+          button: "see offers",
+          imageBg: "/images/main-card.webp",
+        }}
+      />
+      <OfferCard
+        variant="secondary"
+        order="first"
+        data={{
+          title: "brand new style",
+          description: "Popular clothing brands",
+          linkTo: "/catalog",
+          button: "see offers",
+          imageBg: "/images/card-2.webp",
+        }}
+      />
+    </div>
+  );
+};
+
+export default OfferCardSection;
--- components/organism/index.js
+++ components/organism/index.js
@@ -1,1 +1,2 @@
 export { default as HeroSlider } from "./slider/heroSlider";
+export { default as OfferCardSection } from "./card/offerCardSection";
