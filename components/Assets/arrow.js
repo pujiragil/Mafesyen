c4231b7995211fa6c6faf679a2e1f66c497f52bf
pujiragil <ragilsg164@gmail.com>
2023-06-22 23:06:53 +0700

    created svg component

--- components/Assets/arrow.js
+++ components/Assets/arrow.js
@@ -31,3 +31,37 @@ export const ArrowNavigationLeft = ({ className }) => (
     />
   </svg>
 );
+
+export const ArrowCatalogRight = ({ className }) => (
+  <svg
+    className={className}
+    width="8"
+    height="14"
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.543701 12.6L6.20038 6.9433L0.543703 1.28662"
+      stroke="#828282"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
+
+export const ArrowCatalogLeft = ({ className }) => (
+  <svg
+    className={className}
+    width="8"
+    height="14"
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M7.45624 1.40002L1.79956 7.0567L7.45624 12.7134"
+      stroke="#828282"
+      strokeWidth="1.5"
+    />
+  </svg>
+);
