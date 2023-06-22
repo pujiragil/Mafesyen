0f35551eb758a05e9a1ae5a0e82d3cbd5731ce63
pujiragil <ragilsg164@gmail.com>
2023-06-22 23:06:27 +0700

    added color variant

--- components/atoms/head/bigHead.js
+++ components/atoms/head/bigHead.js
@@ -2,13 +2,17 @@ import React from "react";
 import { cva } from "class-variance-authority";
 import { cn } from "@/utils/utils";
 
-const bigHead = cva("font-oswald uppercase text-black", {
+const bigHead = cva("font-oswald uppercase", {
   variants: {
     variant: {
       hero: "text-[2rem] md:text-[3.5rem] lg:text-[4.5rem]",
       "primary-card": "text-2xl md:text-[3rem] lg:text-[4rem]",
       "secondary-card": "text-base md:text-[2rem] lg:text-[2.5rem]",
-      banner: "text-sm md:text-[3rem]",
+      banner: "text-base md:text-[1.5rem] lg:text-[2rem]",
+    },
+    color: {
+      black: "text-black",
+      white: "text-white",
     },
   },
   compoundVariants: [
@@ -25,13 +29,16 @@ const bigHead = cva("font-oswald uppercase text-black", {
       className: "font-medium",
     },
   ],
+  defaultVariants: {
+    color: "black",
+  },
 });
 
-const BigHead = ({ type, variant, className, children, ...props }) => {
+const BigHead = ({ type, variant, color, className, children, ...props }) => {
   return React.createElement(
     type,
     {
-      className: cn(bigHead({ variant, className })),
+      className: cn(bigHead({ variant, color, className })),
       ...props,
     },
     children
