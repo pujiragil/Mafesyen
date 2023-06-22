03ebaed7985efc17a3bf2c33f74c2ab37442a9a9
pujiragil <ragilsg164@gmail.com>
2023-06-22 23:05:23 +0700

    remove unnecessary tag

--- components/Catalog/catalogContainer.js
+++ components/Catalog/catalogContainer.js
@@ -38,13 +38,10 @@ const ProductPrice = dynamic(
 const CatalogSection = () => {
   return (
     <SectionLayout>
-      <div className="mx-3.5 gap-x-8 font-oswald lg:mb-8 lg:flex">
-        <h3 className="mb-4 text-lg md:text-xl lg:hidden">Shop Some Wear:</h3>
-        <div className="hidden w-3/12 lg:block">
-          <CatalogSidebar />
-        </div>
+      <div className="mx-3.5 grid-cols-5 gap-x-8 font-oswald lg:mb-8 lg:grid">
+        <CatalogSidebar />
 
-        <div className="w-full lg:pb-[18px]">
+        <div className="w-full lg:col-span-4 lg:pb-[18px]">
           <div className="mb-[18px] grid grid-cols-2 gap-3.5 sm:grid-cols-3 md:grid-cols-4 lg:mb-12 lg:gap-6">
             {dresses.map((dress) => (
               <ProductCard
