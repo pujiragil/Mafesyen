a3de97d20c1ab0e8230d4e5662873d0c5ced82c0
pujiragil <ragilsg164@gmail.com>
2023-06-22 23:02:19 +0700

    applied BannerCardSection component

--- pages/catalog.js
+++ pages/catalog.js
@@ -1,12 +1,12 @@
-import { SingleCard } from "../components/Card"
-import Filter from "../components/Filter"
-import BaseLayout from "../components/Layout/baseLayout"
+import { BannerCardSection } from "@/components/molecules";
+import Filter from "../components/Filter";
+import BaseLayout from "../components/Layout/baseLayout";
 
 export default function Catalog() {
   return (
     <BaseLayout title={"Mafesyen - Catalog Page"}>
-      <SingleCard margin={"m-3.5"} bgLink={"/images/banner-2.png"} />
-      <Filter/>
+      <BannerCardSection imageBg="/images/card-4.webp" />
+      <Filter />
     </BaseLayout>
-  )
+  );
 }
