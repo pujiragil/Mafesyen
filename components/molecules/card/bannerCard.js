27dc569ab0ea7a37be1b2d61752aa289460e3bd2
pujiragil <ragilsg164@gmail.com>
2023-06-22 22:54:45 +0700

    created BannerCard component

---
+++ components/molecules/card/bannerCard.js
@@ -0,0 +1,36 @@
+import SectionLayout from "@/components/Layout/sectionLayout";
+import { BigHead, ButtonLink, Text } from "@/components/atoms";
+import Image from "next/image";
+
+const BannerCardSection = ({ imageBg }) => {
+  return (
+    <SectionLayout>
+      <div className="min-h-36 relative z-10 mx-3.5 grid grid-cols-[2fr_1fr] p-7 md:h-56 lg:h-72 lg:place-items-center">
+        <div className="flex flex-col gap-2">
+          <BigHead variant="banner" type="h2">
+            shoping without limits.
+          </BigHead>
+          <Text size="xs/lg" className="md:w-9/12 lg:hidden">
+            Choose the best option for you, and it does not matter whether you
+            are.
+          </Text>
+          <Text size="xs/lg" className="hidden md:w-9/12 lg:inline">
+            You can choose the best option for you, and it does not matter
+            whether you are in Prague or San Francisco. We will deliver your
+            purchase anywhere!
+          </Text>
+          <ButtonLink href="/catalog">shop now</ButtonLink>
+        </div>
+        <Image
+          src={imageBg}
+          width={1878}
+          height={467}
+          alt="banner"
+          className="absolute top-0 left-0 -z-10 h-full w-full object-cover"
+        />
+      </div>
+    </SectionLayout>
+  );
+};
+
+export default BannerCardSection;
--- components/molecules/index.js
+++ components/molecules/index.js
@@ -1,5 +1,6 @@
 export * from "./card/productCard";
 export { default as BrandSection } from "./brand";
+export { default as BannerCardSection } from "./card/bannerCard";
 export { default as SliderItem } from "./slider/sliderItem";
 export { default as SliderWrapper } from "./slider/sliderWrapper";
 export { default as OfferCard } from "./card/offerCard";
