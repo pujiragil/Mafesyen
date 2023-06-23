3439f6854e1ffb48e9e2dc2b0f6aaf62a33d3e95
pujiragil <ragilsg164@gmail.com>
2023-06-23 22:57:39 +0700

    fix BannerCardSection prop

--- pages/catalog.js
+++ pages/catalog.js
@@ -5,7 +5,13 @@ import BaseLayout from "../components/Layout/baseLayout";
 export default function Catalog() {
   return (
     <BaseLayout title={"Mafesyen - Catalog Page"}>
-      <BannerCardSection imageBg="/images/card-4.webp" />
+      <BannerCardSection
+        title="shoping without limits."
+        desc="You can choose the best option for you, and it does not matter whether you are in Prague or San Francisco. We will deliver your purchase anywhere!"
+        btnHref="/catalog"
+        btnText="see offers"
+        imgBg="/images/card-4.webp"
+      />
       <Filter />
     </BaseLayout>
   );
