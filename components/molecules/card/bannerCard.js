4421bc171a24297909d30d6b50d05139d97d8c71
pujiragil <ragilsg164@gmail.com>
2023-06-23 21:39:28 +0700

    added new prop to BannerCardSection component

--- components/molecules/card/bannerCard.js
+++ components/molecules/card/bannerCard.js
@@ -2,33 +2,30 @@ import SectionLayout from "@/components/Layout/sectionLayout";
 import { BigHead, ButtonLink, Text } from "@/components/atoms";
 import Image from "next/image";
 
-const BannerCardSection = ({ imageBg, color = "black" }) => {
+const BannerCardSection = ({
+  imgBg,
+  color = "black",
+  title,
+  desc,
+  btnText,
+  btnHref,
+}) => {
   return (
     <SectionLayout>
-      <div className="min-h-36 relative z-10 mx-3.5 grid grid-cols-[2fr_1fr] p-7 md:h-56 lg:h-72 lg:place-items-center">
+      <div className="min-h-36 relative z-10 mx-3.5 grid grid-cols-[3fr_1fr] p-7 md:h-56 md:grid-cols-[2fr_1fr] lg:h-72 lg:place-items-center">
         <div className="flex flex-col gap-2">
           <BigHead color={color} variant="banner" type="h2">
-            shoping without limits.
+            {title}
           </BigHead>
-          <Text color={color} size="xs/lg" className="md:w-9/12 lg:hidden">
-            Choose the best option for you, and it does not matter whether you
-            are.
+          <Text color={color} size="xxs/base" className="text-[10px] md:w-9/12">
+            {desc}
           </Text>
-          <Text
-            color={color}
-            size="xs/lg"
-            className="hidden md:w-9/12 lg:inline"
-          >
-            You can choose the best option for you, and it does not matter
-            whether you are in Prague or San Francisco. We will deliver your
-            purchase anywhere!
-          </Text>
-          <ButtonLink color={color} href="/catalog">
-            shop now
+          <ButtonLink color={color} href={btnHref}>
+            {btnText}
           </ButtonLink>
         </div>
         <Image
-          src={imageBg}
+          src={imgBg}
           width={1878}
           height={467}
           alt="banner"
