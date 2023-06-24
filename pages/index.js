ae3352776e829efaab94c515ece0503cb09de7d6
pujiragil <ragilsg164@gmail.com>
2023-06-24 20:52:05 +0700

    applied dynamic import for CatalogSliderSection component

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,26 @@ import Blog from "../components/Blog";
 import { BannerCardSection, BrandSection } from "@/components/molecules";
 import CatalogSection from "../components/Catalog/catalogContainer";
 import BaseLayout from "../components/Layout/baseLayout";
-import { CatalogSliderSection } from "@/components/templates";
 import { HeroSliderSection, OfferCardSection } from "@/components/organism";
+import dynamic from "next/dynamic";
+import { CatalogSliderSkeleton } from "@/components/templates";
+
+const CatalogSliderSection = dynamic(
+  () =>
+    import("@/components/templates").then((mod) => mod.CatalogSliderSection),
+  {
+    ssr: false,
+    loading: () => <CatalogSliderSkeleton />,
+  }
+);
 
 export default function Home() {
   return (
     <BaseLayout title={"Mafesyen - Welcome back"}>
-      <HeroSliderSection />
+      {/* <HeroSliderSection /> */}
       <BrandSection />
       <OfferCardSection />
-      <CatalogSection />
+      {/* <CatalogSection /> */}
       <BannerCardSection
         title="shoping without limits."
         desc="You can choose the best option for you, and it does not matter whether you are in Prague or San Francisco. We will deliver your purchase anywhere!"
