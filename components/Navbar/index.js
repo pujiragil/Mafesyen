74d1fab20e72245ec5229a4a03d795a637bd7878
pujiragil <ragilsg164@gmail.com>
2023-06-27 23:18:36 +0700

    applied className to custom svg component

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { useState } from "react";
-import { navLinks } from "@/utils/links";
+import data from "@/utils/data.json";
 import { Head } from "../Global";
 import NavDropdown from "./navDropdown";
-import { HamburgerIcon, SearchIcon } from "../Assets/navbar";
+import { CartIcon, HamburgerIcon, SearchIcon } from "../Assets/navbar";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +15,10 @@ export default function Navbar() {
           <div className="flex h-14 items-center md:justify-between">
             {/* NOTE hamburger */}
             <div className="w-1/4 md:hidden">
-              <HamburgerIcon onClick={() => setIsOpen(true)} />
+              <HamburgerIcon
+                className="h-5 w-5 cursor-pointer"
+                onClick={() => setIsOpen(true)}
+              />
             </div>
 
             <div className="flex w-2/4 justify-center md:w-auto lg:w-3/12 lg:justify-start">
@@ -25,9 +28,9 @@ export default function Navbar() {
             </div>
 
             <div className="flex w-1/4 items-center justify-end gap-x-5 md:hidden">
-              <SearchIcon />
+              <SearchIcon className="h-5 w-5 cursor-pointer" />
               <span className="relative">
-                <img src="/cart.svg" alt="cart-icon" />
+                <CartIcon className="h-5 w-5" />
                 <span className="absolute -top-1.5 -right-1.5 bg-[#EB5757] px-1 text-[10px]">
                   2
                 </span>
@@ -47,11 +50,12 @@ export default function Navbar() {
 }
 
 function NavLinks() {
+  const links = data.navbar.links;
   return (
     <>
       <ul className="hidden items-center gap-x-4 uppercase md:flex lg:w-6/12 lg:justify-center">
-        {navLinks.map((link) => (
-          <li key={link.to}>
+        {links.map((link) => (
+          <li key={link.id}>
             <LinkItem to={link.to} name={link.name} />
           </li>
         ))}
