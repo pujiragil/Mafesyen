9f62bfa992fa818e161918042171245cf1f3a5a0
pujiragil <ragilsg164@gmail.com>
2023-06-27 23:07:44 +0700

    applied built in next/font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,28 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import { Roboto, Oswald } from "next/font/google";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "300",
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+const oswald = Oswald({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-oswald",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <main className={`${roboto.variable} ${oswald.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
