994f6b0a7b0a8e7efb7024c15688a2adf8edb4bc
pujiragil <ragilsg164@gmail.com>
2023-06-27 23:12:33 +0700

    created custom svg component

--- components/Assets/navbar.js
+++ components/Assets/navbar.js
@@ -1,8 +1,8 @@
-export const HamburgerIcon = ({ onClick }) => {
+export const HamburgerIcon = ({ onClick, className }) => {
   return (
     <svg
       onClick={onClick}
-      className="cursor-pointer"
+      className={className}
       width="20"
       height="15"
       viewBox="0 0 20 15"
@@ -16,10 +16,10 @@ export const HamburgerIcon = ({ onClick }) => {
   );
 };
 
-export const SearchIcon = () => {
+export const SearchIcon = ({ className }) => {
   return (
     <svg
-      className="cursor-pointer"
+      className={className}
       width="20"
       height="20"
       viewBox="0 0 20 20"
@@ -41,3 +41,27 @@ export const SearchIcon = () => {
     </svg>
   );
 };
+
+export const CartIcon = ({ className }) => (
+  <svg
+    className={className}
+    width="18"
+    height="22"
+    viewBox="0 0 18 22"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.26297 6.59995L2.54533 18.8H15.4547L14.737 6.59995H3.26297ZM17 4.19995H1L0 21.2H18L17 4.19995Z"
+      fill="white"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M6.20078 2.4V4.2H3.80078V2.2C3.80078 0.984973 4.78576 0 6.00078 0H12.0008C13.2158 0 14.2008 0.984974 14.2008 2.2V4.2H11.8008V2.4H6.20078Z"
+      fill="white"
+    />
+  </svg>
+);
