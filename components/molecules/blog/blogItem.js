7c981713651527f561d824fb8a4b344d99ccd9a4
pujiragil <ragilsg164@gmail.com>
2023-06-28 22:58:09 +0700

    created BlogItem component

---
+++ components/molecules/blog/blogItem.js
@@ -0,0 +1,20 @@
+import { Category } from "@/components/atoms";
+
+const BlogItem = ({ title, desc, date, highlight }) => {
+  return (
+    <article className="flex h-56 flex-col  justify-between bg-white p-3 font-oswald md:h-72 lg:h-[350px]">
+      <div>
+        <Category intent="secondary" value={highlight} />
+        <h4 className="mt-1 mb-3.5 text-sm md:text-base lg:text-lg">{title}</h4>
+        <p className="font-roboto text-[10px] md:text-xs lg:text-sm">{desc}</p>
+      </div>
+      <div className="border-t-2 border-black py-1">
+        <p className="font-roboto text-[10px] md:text-xs lg:text-sm">
+          {date} by Puji Ragil
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default BlogItem;
--- components/molecules/index.js
+++ components/molecules/index.js
@@ -1,7 +1,10 @@
 export * from "./card/productCard";
 export { default as BrandSection } from "./brand";
 export { default as BannerCardSection } from "./card/bannerCard";
+export { default as BlogItem } from "./blog/blogItem";
 export { default as EventCardSection } from "./card/eventCard";
+export { default as Navbar } from "./navbar/";
+export { default as NavMobile } from "./navbar/navMobile";
 export { default as SliderItem } from "./slider/sliderItem";
 export { default as SliderWrapper } from "./slider/sliderWrapper";
 export { default as OfferCard } from "./card/offerCard";
