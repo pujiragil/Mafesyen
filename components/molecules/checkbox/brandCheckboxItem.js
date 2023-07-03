3106f853f2c11245cc2c54c4db6b6937c02edd17
pujiragil <ragilsg164@gmail.com>
2023-07-03 22:10:06 +0700

    created BrandCheckboxItem

---
+++ components/molecules/checkbox/brandCheckboxItem.js
@@ -0,0 +1,18 @@
+import { BaseCheckbox } from "@/components/atoms";
+import { useState } from "react";
+
+const BrandCheckboxItem = ({ children }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <label className="block space-x-2">
+      <BaseCheckbox
+        checked={isChecked}
+        onChecked={() => setIsChecked((prevCheck) => !prevCheck)}
+      />
+      <span className="text-roboto text-xs uppercase">{children}</span>
+    </label>
+  );
+};
+
+export default BrandCheckboxItem;
