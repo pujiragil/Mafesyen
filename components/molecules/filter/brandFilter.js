6afe3c199fc07bb6a1acd2e26e47836ac47b4daf
pujiragil <ragilsg164@gmail.com>
2023-07-04 22:44:04 +0700

    applied DropdownWrapper component

--- components/molecules/filter/brandFilter.js
+++ components/molecules/filter/brandFilter.js
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { cn } from "@/utils/utils";
-import BrandCheckboxItem from "../checkbox/brandCheckboxItem";
+import { BrandCheckboxItem } from "@/components/molecules";
+import { DropdownWrapper } from "@/components/atoms";
 
 const BrandFilter = ({ data }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="flex flex-col">
-      <div className={`${isOpen ? "space-y-3" : ""} p-3`}>
-        <div className="flex items-center justify-between">
+      <div className={cn(isOpen && "space-y-3", "p-3")}>
+        <div
+          onClick={() => setIsOpen((prevOpen) => !prevOpen)}
+          className="flex cursor-pointer items-center justify-between"
+        >
           <p className="font-oswald text-sm text-black">Brand</p>
           <svg
             className="w-3 cursor-pointer"
@@ -23,22 +27,14 @@ const BrandFilter = ({ data }) => {
           </svg>
         </div>
 
-        <div
-          className={cn(
-            "grid",
-            "transition-all duration-300 ease-in-out",
-            isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]"
-          )}
-        >
-          <div className="space-y-2 overflow-hidden">
-            {data.map((brand) => (
-              <BrandCheckboxItem key={brand}>{brand}</BrandCheckboxItem>
-            ))}
-          </div>
-        </div>
+        <DropdownWrapper isOpen={isOpen} className="space-y-2">
+          {data.map((brand) => (
+            <BrandCheckboxItem key={brand}>{brand}</BrandCheckboxItem>
+          ))}
+        </DropdownWrapper>
       </div>
     </div>
   );
 };
 
-export default BrandFilter;
+export default memo(BrandFilter);
--- components/molecules/index.js
+++ components/molecules/index.js
@@ -1,13 +1,21 @@
 export * from "./card/productCard";
+
 export { default as BrandCheckboxItem } from "./checkbox/brandCheckboxItem";
+export { default as DressCheckboxItem } from "./checkbox/dressCheckboxItem";
+
 export { default as BrandFilter } from "./filter/brandFilter";
+export { default as DressFilter } from "./filter/dressFilter";
+
 export { default as BrandSection } from "./brand";
-export { default as BannerCardSection } from "./card/bannerCard";
 export { default as BlogItem } from "./blog/blogItem";
+
+export { default as BannerCardSection } from "./card/bannerCard";
 export { default as EventCardSection } from "./card/eventCard";
-export { default as Navbar } from "./navbar/";
-export { default as NavMobile } from "./navbar/navMobile";
-export { default as SliderItem } from "./slider/sliderItem";
-export { default as SliderWrapper } from "./slider/sliderWrapper";
 export { default as OfferCard } from "./card/offerCard";
 export { default as ProductCardSkeleton } from "./card/productCardSkeleton";
+
+export { default as Navbar } from "./navbar/";
+export { default as NavMobile } from "./navbar/navMobile";
+
+export { default as SliderItem } from "./slider/sliderItem";
+export { default as SliderWrapper } from "./slider/sliderWrapper";
