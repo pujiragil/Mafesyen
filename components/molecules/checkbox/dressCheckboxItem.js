7d91be2995fec069cf00cfb17764b7f5c14011a4
pujiragil <ragilsg164@gmail.com>
2023-07-04 22:42:52 +0700

    created dressCheckboxItem component

---
+++ components/molecules/checkbox/dressCheckboxItem.js
@@ -0,0 +1,20 @@
+import { BaseCheckbox } from "@/components/atoms";
+import { useState } from "react";
+
+const DressCheckboxItem = ({ children }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <label htmlFor="item" className="block space-x-2">
+      <BaseCheckbox
+        checked={isChecked}
+        onChecked={() => setIsChecked((prevCheck) => !prevCheck)}
+      />
+      <span id="item" className="text-roboto text-[10px] uppercase">
+        {children}
+      </span>
+    </label>
+  );
+};
+
+export default DressCheckboxItem;
