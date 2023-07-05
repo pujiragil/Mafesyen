3371dd3c551b3014a61e598889469a9e65d87de2
pujiragil <ragilsg164@gmail.com>
2023-07-05 22:46:16 +0700

    change display element

--- components/molecules/checkbox/brandCheckboxItem.js
+++ components/molecules/checkbox/brandCheckboxItem.js
@@ -5,7 +5,7 @@ const BrandCheckboxItem = ({ children }) => {
   const [isChecked, setIsChecked] = useState(false);
 
   return (
-    <label htmlFor="item" className="block space-x-2">
+    <label htmlFor="item" className="flex items-center gap-2">
       <BaseCheckbox
         checked={isChecked}
         onChecked={() => setIsChecked((prevCheck) => !prevCheck)}
