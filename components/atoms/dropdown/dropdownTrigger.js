9b29beb98be233386f3ba1995e1b70bbfd3328cf
pujiragil <ragilsg164@gmail.com>
2023-07-05 22:45:25 +0700

    create DropdownTrigger component

---
+++ components/atoms/dropdown/dropdownTrigger.js
@@ -0,0 +1,16 @@
+import { memo } from "react";
+import { DropdownClose, DropdownOpen } from "@/components/assets";
+
+const DropdownTrigger = ({ open, onOpen, children }) => {
+  return (
+    <div
+      onClick={onOpen}
+      className="flex h-full cursor-pointer items-center justify-between"
+    >
+      <p className="font-oswald text-sm text-black">{children}</p>
+      {open ? <DropdownClose /> : <DropdownOpen />}
+    </div>
+  );
+};
+
+export default memo(DropdownTrigger);
