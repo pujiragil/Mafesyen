8a656fefacec98e28ad96d9c1eadc754ffb91347
pujiragil <ragilsg164@gmail.com>
2023-07-05 22:47:35 +0700

    applied DropdownTrigger component

--- components/molecules/filter/brandFilter.js
+++ components/molecules/filter/brandFilter.js
@@ -1,31 +1,18 @@
-import { memo, useState } from "react";
+import { memo } from "react";
 import { cn } from "@/utils/utils";
 import { BrandCheckboxItem } from "@/components/molecules";
-import { DropdownWrapper } from "@/components/atoms";
+import { DropdownWrapper, DropdownTrigger } from "@/components/atoms";
+import useDropdown from "@/hooks/useDropdown";
 
 const BrandFilter = ({ data }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useDropdown();
 
   return (
     <div className="flex flex-col">
-      <div className={cn(isOpen && "space-y-3", "p-3")}>
-        <div
-          onClick={() => setIsOpen((prevOpen) => !prevOpen)}
-          className="flex cursor-pointer items-center justify-between"
-        >
-          <p className="font-oswald text-sm text-black">Brand</p>
-          <svg
-            className="w-3 cursor-pointer"
-            onClick={() => setIsOpen((prevOpen) => !prevOpen)}
-            width="12"
-            height="2"
-            viewBox="0 0 12 2"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect width="12" height="2" fill="#3F3F3F" />
-          </svg>
-        </div>
+      <div className={cn(isOpen && "space-y-3", "py-3")}>
+        <DropdownTrigger open={isOpen} onOpen={setIsOpen}>
+          Brands
+        </DropdownTrigger>
 
         <DropdownWrapper isOpen={isOpen} className="space-y-2">
           {data.map((brand) => (
