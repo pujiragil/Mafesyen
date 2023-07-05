f965a9054fdd0e5bffdfe5ee488522538cbe998a
pujiragil <ragilsg164@gmail.com>
2023-07-05 22:44:05 +0700

    create custom svg component

---
+++ components/assets/dropdown.js
@@ -0,0 +1,32 @@
+export const DropdownClose = ({ className }) => (
+  <svg
+    className={`w-3 cursor-pointer ${className}`}
+    width="12"
+    height="2"
+    viewBox="0 0 12 2"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="12" height="2" fill="#3F3F3F" />
+  </svg>
+);
+
+export const DropdownOpen = ({ className }) => (
+  <svg
+    className={`w-3 cursor-pointer ${className}`}
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y="5" width="12" height="2" fill="#3F3F3F" />
+    <rect
+      x="7"
+      width="12"
+      height="2"
+      transform="rotate(90 7 0)"
+      fill="#3F3F3F"
+    />
+  </svg>
+);
--- components/assets/index.js
+++ components/assets/index.js
@@ -1,3 +1,4 @@
 export * from "./arrow";
+export * from "./dropdown";
 export * from "./navbar";
 export * from "./mafesyen";
