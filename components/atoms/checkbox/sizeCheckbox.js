a6a6796280708c07a5cd2440c2feace31a5d556c
pujiragil <ragilsg164@gmail.com>
2023-07-05 23:28:28 +0700

    created sizeCheckbox component

---
+++ components/atoms/checkbox/sizeCheckbox.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { cn } from "@/utils/utils";
+
+const SizeCheckbox = ({ children }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <span
+      onClick={() => setIsChecked((prevCheck) => !prevCheck)}
+      className={cn(
+        "block",
+        "h-10 w-10",
+        "uppercase",
+        "cursor-pointer",
+        "font-oswald text-sm",
+        "grid place-items-center",
+        isChecked
+          ? "border-2 border-[#000000] text-black"
+          : "border border-[#C4C4C4] text-[#828282]"
+      )}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default SizeCheckbox;
