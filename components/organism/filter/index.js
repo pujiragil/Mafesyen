35c4820e1756de0c75b2f31d5f5108451efda17e
pujiragil <ragilsg164@gmail.com>
2023-07-05 22:49:49 +0700

    used custom hooks

--- components/organism/filter/index.js
+++ components/organism/filter/index.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { cn } from "@/utils/utils";
+import { useDropdown } from "@/hooks";
+
 import { BrandFilter, DressFilter } from "@/components/molecules";
 import SectionLayout from "@/components/Layout/sectionLayout";
 import { DropdownWrapper } from "@/components/atoms";
@@ -7,19 +9,29 @@ const brands = ["state", "cooper", "bardot", "alfani", "cece", "donna ricco"];
 const dresses = ["short", "knee length", "high low", "long", "mid"];
 
 const FilterTab = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useDropdown();
 
   return (
     <SectionLayout>
       <div className="m-3.5">
         <div className="w-full border border-black">
           <div
-            onClick={() => setIsOpen((prevOpen) => !prevOpen)}
-            className="flex cursor-pointer items-center justify-between bg-black p-3"
+            onClick={setIsOpen}
+            className={cn(
+              "p-3",
+              "cursor-pointer",
+              "bg-black",
+              "flex items-center justify-between"
+            )}
           >
             <p className="font-oswald text-sm uppercase text-white">filter</p>
             <svg
-              className="w-3 cursor-pointer"
+              className={cn(
+                "w-3",
+                "cursor-pointer",
+                "transition-transform duration-300 ease-out",
+                isOpen ? "rotate-180" : "rotate-0"
+              )}
               width="10"
               height="6"
               viewBox="0 0 10 6"
@@ -30,13 +42,15 @@ const FilterTab = () => {
             </svg>
           </div>
 
-          <DropdownWrapper isOpen={isOpen}>
-            <BrandFilter data={brands} />
-          </DropdownWrapper>
+          <div className="divide-y divide-[#F0F1F2] px-3">
+            <DropdownWrapper isOpen={isOpen}>
+              <BrandFilter data={brands} />
+            </DropdownWrapper>
 
-          <DropdownWrapper isOpen={isOpen}>
-            <DressFilter data={dresses} />
-          </DropdownWrapper>
+            <DropdownWrapper isOpen={isOpen}>
+              <DressFilter data={dresses} />
+            </DropdownWrapper>
+          </div>
         </div>
       </div>
     </SectionLayout>
