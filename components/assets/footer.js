89680ac63a99d56b703ee2dcae912729d5cbe53b
pujiragil <ragilsg164@gmail.com>
2023-07-08 00:40:52 +0700

    fix assets spelling

--- components/assets/footer.js
+++ components/assets/footer.js
@@ -8,8 +8,8 @@ export const FooterDropdownOpen = ({ className, ...props }) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path d="M9.5 0V9V18" stroke="#3F3F3F" stroke-width="1.4" />
-    <path d="M19 9L9.5 9L-4.76837e-07 9" stroke="#3F3F3F" stroke-width="1.4" />
+    <path d="M9.5 0V9V18" stroke="#3F3F3F" strokeWidth="1.4" />
+    <path d="M19 9L9.5 9L-4.76837e-07 9" stroke="#3F3F3F" strokeWidth="1.4" />
   </svg>
 );
 
@@ -26,7 +26,7 @@ export const FooterDropdownClose = ({ className, ...props }) => (
     <path
       d="M19 1L9.5 1L-4.76837e-07 0.999999"
       stroke="white"
-      stroke-width="1.4"
+      strokeWidth="1.4"
     />
   </svg>
 );
