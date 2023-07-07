2bfb667b520b63489586399336f6adb55d7305b6
pujiragil <ragilsg164@gmail.com>
2023-07-08 00:30:04 +0700

    created footerSubmenu component

---
+++ components/atoms/footer/footerSubmenu.js
@@ -0,0 +1,9 @@
+const FooterSubmenu = ({ children }) => {
+  return (
+    <h4 className="font-oswald text-sm font-medium uppercase tracking-[0.5px] text-white md:text-base">
+      {children}
+    </h4>
+  );
+};
+
+export default FooterSubmenu;
