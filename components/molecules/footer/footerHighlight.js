3f460202fba82ebcf3622cedfefdaacd3536523b
pujiragil <ragilsg164@gmail.com>
2023-07-16 22:38:16 +0700

    created dummy data for highlight

--- components/molecules/footer/footerHighlight.js
+++ components/molecules/footer/footerHighlight.js
@@ -1,16 +1,22 @@
 import SectionLayout from "@/components/Layout/sectionLayout";
 import { FooterHighlightItem } from "@/components/molecules";
 
+const highlights = [
+  "Duties and Taxes Guaranteed",
+  "Free Express Shipping",
+  "Customer Love",
+  "Easy Returns",
+  "Secure Payment",
+];
+
 const FooterHighlight = () => {
   return (
     <SectionLayout background="bg-[#E6F1FA]">
       <div className="mx-3.5 grid place-items-center p-5 md:h-28 md:p-6">
         <div className="flex flex-col gap-2 md:w-full md:flex-row md:justify-between">
-          <FooterHighlightItem>Duties and Taxes Guaranteed</FooterHighlightItem>
-          <FooterHighlightItem>Free Express Shipping</FooterHighlightItem>
-          <FooterHighlightItem>Customer Love</FooterHighlightItem>
-          <FooterHighlightItem>Easy Returns</FooterHighlightItem>
-          <FooterHighlightItem>Secure Payment</FooterHighlightItem>
+          {highlights.map((highlight, idx) => (
+            <FooterHighlightItem key={idx}>{highlight}</FooterHighlightItem>
+          ))}
         </div>
       </div>
     </SectionLayout>
