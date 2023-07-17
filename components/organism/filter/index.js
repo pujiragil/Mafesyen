d8639da6b8f19e8105d633090c92531f30a4815a
pujiragil <ragilsg164@gmail.com>
2023-07-17 23:05:27 +0700

    added conditional classes for filterTab

--- components/organism/filter/index.js
+++ components/organism/filter/index.js
@@ -40,52 +40,54 @@ const FilterTab = () => {
   const [isOpen, setIsOpen] = useDropdown();
 
   return (
-    <SectionLayout>
-      <div className="m-3.5">
-        <div className="w-full border border-black">
-          <div
-            onClick={setIsOpen}
-            className={cn(
-              "p-3",
-              "cursor-pointer",
-              "bg-black",
-              "flex items-center justify-between"
-            )}
-          >
-            <p className="font-oswald text-sm uppercase text-white">filter</p>
-            <svg
-              className={cn(
-                "w-3",
-                "cursor-pointer",
-                "transition-transform duration-300 ease-out",
-                isOpen ? "rotate-180" : "rotate-0"
-              )}
-              width="10"
-              height="6"
-              viewBox="0 0 10 6"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M9 5L5 1L1 5" stroke="white" />
-            </svg>
-          </div>
-
-          <div className="divide-y divide-[#F0F1F2] px-3">
-            <DropdownWrapper isOpen={isOpen}>
-              <BrandFilter data={brands} />
-            </DropdownWrapper>
-
-            <DropdownWrapper isOpen={isOpen}>
-              <DressFilter data={dresses} />
-            </DropdownWrapper>
-
-            <DropdownWrapper isOpen={isOpen}>
-              <SizeFilter data={sizes} />
-            </DropdownWrapper>
-          </div>
-        </div>
+    <div
+      className={cn(
+        "w-full border",
+        isOpen ? "border-black" : "border-[#C4C4C4]"
+      )}
+    >
+      <div
+        onClick={setIsOpen}
+        className={cn(
+          "p-3",
+          "cursor-pointer",
+          "flex items-center justify-between",
+          "transition-colors duration-300 ease-out",
+          isOpen ? "bg-black text-white" : "bg-white text-black"
+        )}
+      >
+        <p className="font-oswald text-sm uppercase">filter</p>
+        <svg
+          className={cn(
+            "w-3",
+            "cursor-pointer",
+            "transition-transform duration-300 ease-out",
+            isOpen ? "rotate-180 stroke-white" : "rotate-0 stroke-black"
+          )}
+          width="10"
+          height="6"
+          viewBox="0 0 10 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9 5L5 1L1 5" />
+        </svg>
       </div>
-    </SectionLayout>
+
+      <div className="divide-y divide-[#F0F1F2] px-3">
+        <DropdownWrapper isOpen={isOpen}>
+          <BrandFilter data={brands} />
+        </DropdownWrapper>
+
+        <DropdownWrapper isOpen={isOpen}>
+          <DressFilter data={dresses} />
+        </DropdownWrapper>
+
+        <DropdownWrapper isOpen={isOpen}>
+          <SizeFilter data={sizes} />
+        </DropdownWrapper>
+      </div>
+    </div>
   );
 };
 
