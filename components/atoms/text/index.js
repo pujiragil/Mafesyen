2936b87ac370b46680c0993ad4e5c75a55241743
pujiragil <ragilsg164@gmail.com>
2023-07-17 23:04:02 +0700

    added new variant size

--- components/atoms/text/index.js
+++ components/atoms/text/index.js
@@ -5,8 +5,10 @@ const text = cva("font-light text-black", {
   variants: {
     size: {
       xs: "text-xs",
+      "xxs/sm/base": "text-[10px] md:text-sm lg:text-base",
       "xxs/base": "text-[10px] md:text-sm lg:text-base",
       "xs/sm": "text-xs md:text-sm",
+      "xs/sm/base": "text-xs md:text-sm lg:text-base",
       "xs/lg": "text-xs md:text-base lg:text-lg",
       "xs/2xl": "text-xs md:text-2xl",
       "sm/2xl": "text-sm md:text-2xl",
