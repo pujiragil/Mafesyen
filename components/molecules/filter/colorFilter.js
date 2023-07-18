f0961537e84b02cc20dce4b3bdf37a1494009462
pujiragil <ragilsg164@gmail.com>
2023-07-18 23:57:14 +0700

    created ColorFilter dropdown component

---
+++ components/molecules/filter/colorFilter.js
@@ -0,0 +1,21 @@
+import { useDropdown } from "@hooks";
+import { cn } from "@/utils/utils";
+import { ColorsCheckbox } from "@/components/molecules";
+import { DropdownTrigger, DropdownWrapper } from "@/components/atoms";
+
+const ColorFilter = ({ data }) => {
+  const [isOpen, setIsOpen] = useDropdown();
+  return (
+    <div className={cn(isOpen && "space-y-3", "py-3")}>
+      <DropdownTrigger open={isOpen} onOpen={setIsOpen}>
+        Colors
+      </DropdownTrigger>
+
+      <DropdownWrapper isOpen={isOpen}>
+        <ColorsCheckbox data={data} />
+      </DropdownWrapper>
+    </div>
+  );
+};
+
+export default ColorFilter;
