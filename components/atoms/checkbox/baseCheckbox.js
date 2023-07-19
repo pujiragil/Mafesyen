d410a6e9ab405c8c9b805d8436e17081bfd3a113
pujiragil <ragilsg164@gmail.com>
2023-07-19 23:53:40 +0700

    added children for BaseCheckbox

--- components/atoms/checkbox/baseCheckbox.js
+++ components/atoms/checkbox/baseCheckbox.js
@@ -1,28 +1,42 @@
 import { cn } from "@/utils/utils";
 
-const BaseCheckbox = ({ checked, onChecked }) => {
+const BaseCheckbox = ({ checked, onChecked, children }) => {
   return (
-    <span
+    <div
       onClick={onChecked}
-      className={cn(
-        checked ? "border-none bg-black" : "border border-[#828282] bg-white",
-        "inline-flex items-center justify-center",
-        "h-4 w-4",
-        "cursor-pointer",
-        "transition-colors duration-200 ease-in-out"
-      )}
+      className="flex w-fit cursor-pointer items-center gap-2"
     >
-      <svg
-        className="h-auto w-3"
-        width="8"
-        height="6"
-        viewBox="0 0 8 6"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+      <span
+        className={cn(
+          checked ? "border-none bg-black" : "border border-[#C4C4C4] bg-white",
+          "inline-flex items-center justify-center",
+          "h-4 w-4",
+          "cursor-pointer",
+          "transition-colors duration-200 ease-in-out"
+        )}
       >
-        <path d="M1 2.5L3.5 5L7.5 1" stroke="white" />
-      </svg>
-    </span>
+        <svg
+          className="h-auto w-3"
+          width="8"
+          height="6"
+          viewBox="0 0 8 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 2.5L3.5 5L7.5 1" stroke="white" />
+        </svg>
+      </span>
+
+      <p
+        className={cn(
+          "font-roboto uppercase",
+          "text-[10px] md:text-xs lg:text-sm",
+          checked ? "text-black" : "text-[#828282]"
+        )}
+      >
+        {children}
+      </p>
+    </div>
   );
 };
 
