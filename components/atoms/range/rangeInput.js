75d5af6c97162f1c0bda965045cce6671cfa0bc5
pujiragil <ragilsg164@gmail.com>
2023-07-24 23:51:48 +0700

    remove memo for rangeInput

--- components/atoms/range/rangeInput.js
+++ components/atoms/range/rangeInput.js
@@ -1,11 +1,9 @@
-import { memo } from "react";
-
 const RangeInput = (props) => {
   const rangeWidth = (props.range / props.maxRange) * 100 + "%";
 
   return (
     <div className="flex h-5 items-center">
-      <div className="relative block h-0.5 lg:h-1 w-full bg-gray-400 before:absolute before:left-0 before:-top-[8px] before:z-0 before:h-[18px] before:w-[5px] before:bg-black">
+      <div className="relative block h-0.5 w-full bg-gray-400 before:absolute before:left-0 before:-top-[8px] before:z-0 before:h-[18px] before:w-[5px] before:bg-black">
         <input
           type="range"
           min={props.minRange}
@@ -25,4 +23,4 @@ const RangeInput = (props) => {
   );
 };
 
-export default memo(RangeInput);
+export default RangeInput;
