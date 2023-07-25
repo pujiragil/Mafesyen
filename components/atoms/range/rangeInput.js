43f24c39941182f520e25c5458f9e1289fd44694
pujiragil <ragilsg164@gmail.com>
2023-07-25 22:36:01 +0700

    change base rangeInput size

--- components/atoms/range/rangeInput.js
+++ components/atoms/range/rangeInput.js
@@ -2,8 +2,8 @@ const RangeInput = (props) => {
   const rangeWidth = (props.range / props.maxRange) * 100 + "%";
 
   return (
-    <div className="flex h-5 items-center">
-      <div className="relative block h-0.5 w-full bg-gray-400 before:absolute before:left-0 before:-top-[8px] before:z-0 before:h-[18px] before:w-[5px] before:bg-black">
+    <div className="flex h-5 items-center overflow-hidden">
+      <div className="relative block h-0.5 w-full bg-gray-400 before:absolute before:left-0 before:-top-[9.5px] before:z-0 before:h-5 before:w-1.5 before:bg-black">
         <input
           type="range"
           min={props.minRange}
