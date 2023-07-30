44dc27e9e815b9d96957ee08bf28db892ea72fc2
pujiragil <ragilsg164@gmail.com>
2023-07-30 23:42:42 +0700

    created ProductPrice component

---
+++ components/atoms/product/productPrice.js
@@ -0,0 +1,9 @@
+const ProductPrice = ({ value }) => {
+  return (
+    <p className="font-oswald text-2xl font-medium uppercase text-black">
+      {value}
+    </p>
+  );
+};
+
+export default ProductPrice;
