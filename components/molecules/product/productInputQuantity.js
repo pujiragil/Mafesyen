1409c36e66e174668626cb273da707aaab90ab52
pujiragil <ragilsg164@gmail.com>
2023-08-01 23:27:25 +0700

    wrap svg component with div

--- components/molecules/product/productInputQuantity.js
+++ components/molecules/product/productInputQuantity.js
@@ -15,22 +15,48 @@ const ProductInputQuantity = ({ label }) => {
   };
 
   return (
-    <div className="space-y-2">
+    <div className="space-y-2 sm:place-self-end md:place-self-auto">
       <ProductLabel>{label}</ProductLabel>
 
       <div className="grid h-12 w-[150px] grid-cols-3 border border-[#C4C4C4]">
-        <div className="grid cursor-pointer place-items-center" onClick={handleDecrement}>
-          <svg width="10" height="2" viewBox="0 0 10 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div
+          className="grid cursor-pointer place-items-center"
+          onClick={handleDecrement}
+        >
+          <svg
+            width="10"
+            height="2"
+            viewBox="0 0 10 2"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <rect width="10" height="2" fill="#C4C4C4" />
           </svg>
         </div>
 
-        <p className="place-self-center font-oswald text-base font-normal">{quantity}</p>
+        <p className="place-self-center font-oswald text-base font-normal">
+          {quantity}
+        </p>
 
-        <div className="grid cursor-pointer place-items-center" onClick={handleIncrement}>
-          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div
+          className="grid cursor-pointer place-items-center"
+          onClick={handleIncrement}
+        >
+          <svg
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <rect y="5" width="12" height="2" fill="#C4C4C4" />
-            <rect x="7" width="12" height="2" transform="rotate(90 7 0)" fill="#C4C4C4" />
+            <rect
+              x="7"
+              width="12"
+              height="2"
+              transform="rotate(90 7 0)"
+              fill="#C4C4C4"
+            />
           </svg>
         </div>
       </div>
