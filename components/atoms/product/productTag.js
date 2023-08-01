a2b1bd93c2fa7f0fad60745cfe9d8091f458eaaa
pujiragil <ragilsg164@gmail.com>
2023-08-01 23:25:42 +0700

    added padding for lg screen

--- components/atoms/product/productTag.js
+++ components/atoms/product/productTag.js
@@ -1,6 +1,6 @@
 const ProductTag = ({ children }) => {
   return (
-    <p className="w-fit bg-[#F0F1F2] px-2 py-1 font-oswald text-sm font-medium uppercase text-[#3F3F3F]">
+    <p className="w-fit bg-[#F0F1F2] px-2 py-1 font-oswald text-sm font-medium uppercase text-[#3F3F3F] lg:text-base lg:px-3">
       {children}
     </p>
   );
