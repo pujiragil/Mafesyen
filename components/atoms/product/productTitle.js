e69852d4c5367c0244b6617fd4bd50ed23a693e7
pujiragil <ragilsg164@gmail.com>
2023-08-01 23:26:47 +0700

    change ProductTitle size for lg screen

--- components/atoms/product/productTitle.js
+++ components/atoms/product/productTitle.js
@@ -1,6 +1,8 @@
 const ProductTitle = ({ children }) => {
   return (
-    <h2 className="font-oswald text-xl font-medium text-black">{children}</h2>
+    <h2 className="font-oswald text-xl font-medium text-black lg:text-4xl">
+      {children}
+    </h2>
   );
 };
 
