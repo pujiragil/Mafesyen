7d671a708e905ff647f53bc1602d890cbf46922e
pujiragil <ragilsg164@gmail.com>
2023-08-04 00:26:19 +0700

    change size

--- components/atoms/product/productPrice.js
+++ components/atoms/product/productPrice.js
@@ -1,6 +1,6 @@
 const ProductPrice = ({ value }) => {
   return (
-    <p className="font-oswald text-2xl font-medium uppercase text-black md:text-2xl">
+    <p className="font-oswald text-xl font-medium uppercase text-black">
       {value}
     </p>
   );
--- components/atoms/product/productTitle.js
+++ components/atoms/product/productTitle.js
@@ -1,6 +1,6 @@
 const ProductTitle = ({ children }) => {
   return (
-    <h2 className="font-oswald text-xl font-medium text-black lg:text-4xl">
+    <h2 className="font-oswald text-2xl font-medium text-black lg:text-4xl">
       {children}
     </h2>
   );
