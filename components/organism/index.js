486b6aeb39e6041f9093f20ee1d622511d263f06
pujiragil <ragilsg164@gmail.com>
2023-08-04 00:27:27 +0700

    create ProductSlider component

--- components/organism/index.js
+++ components/organism/index.js
@@ -1,5 +1,6 @@
 export { default as BlogSection } from "./blog/blogSection";
 export { default as FilterTab } from "./filter";
 export { default as Footer } from "./footer";
-export { default as HeroSliderSection } from "./slider/heroSliderSection";
 export { default as OfferCardSection } from "./card/offerCardSection";
+export { default as ProductSlider } from "./product/productSlider";
+export { default as HeroSliderSection } from "./slider/heroSliderSection";
---
+++ components/organism/product/productSlider.js
@@ -0,0 +1,16 @@
+import { memo, useState } from "react";
+
+import { ProductSliderImage, ProductSliderThumb } from "@/components/molecules";
+
+const ProductSlider = ({ productImages }) => {
+  const [thumbSlider, setThumbSlider] = useState(null);
+
+  return (
+    <div className="block py-3.5 sm:h-[500px] lg:grid lg:h-[560px] lg:grid-cols-[80px_1fr] lg:gap-4">
+      <ProductSliderThumb onSlide={setThumbSlider} data={productImages} />
+      <ProductSliderImage thumb={thumbSlider} data={productImages} />
+    </div>
+  );
+};
+
+export default memo(ProductSlider);
