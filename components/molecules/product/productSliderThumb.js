0f730a2334be24709943e07d59f27be1b38e70ad
pujiragil <ragilsg164@gmail.com>
2023-08-06 23:27:53 +0700

    applied next Image for performance optimization

--- components/molecules/product/productSliderThumb.js
+++ components/molecules/product/productSliderThumb.js
@@ -1,4 +1,5 @@
 import { memo } from "react";
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Thumbs } from "swiper/modules";
 
@@ -15,11 +16,13 @@ const ProductSliderThumb = ({ onSlide, data }) => {
       className="thumbs-swiper hidden h-full w-full overflow-hidden lg:flex"
     >
       {data.map((image) => (
-        <SwiperSlide key={image}>
-          <img
-            src={`/images/dress/${image}`}
-            alt="dress"
-            data={data}
+        <SwiperSlide key={image.id}>
+          <Image
+            width={1920}
+            height={1280}
+            src={`/images/dress/${image.link}`}
+            alt={image.alt}
+            blurDataURL={image.blurDataUrl}
             className="h-24 w-20 cursor-pointer object-cover object-center"
           />
         </SwiperSlide>
