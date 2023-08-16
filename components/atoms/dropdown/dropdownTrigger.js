023d943f1223b97d0cbbeac401e7b991043fe27f
pujiragil <ragilsg164@gmail.com>
2023-08-16 23:39:17 +0700

    added className props

--- components/atoms/dropdown/dropdownTrigger.js
+++ components/atoms/dropdown/dropdownTrigger.js
@@ -1,16 +1,23 @@
 import { memo } from "react";
+import { cn } from "@/utils/utils";
 import { DropdownClose, DropdownOpen } from "@/components/assets";
 
-const DropdownTrigger = ({ open, onOpen, children }) => {
+const DropdownTrigger = ({ open, onOpen, className, children }) => {
   return (
     <div
       onClick={onOpen}
       className="flex h-full cursor-pointer items-center justify-between"
     >
-      <p className="font-oswald text-sm text-black md:text-lg">
+      <p
+        className={cn(
+          "font-oswald",
+          "text-sm text-black md:text-lg",
+          className
+        )}
+      >
         {children}
       </p>
-      
+
       {open ? <DropdownClose /> : <DropdownOpen />}
     </div>
   );
