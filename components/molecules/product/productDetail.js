6a2afc786c0d0a0585ebb5bec5ba96bec795b320
pujiragil <ragilsg164@gmail.com>
2023-08-21 23:53:12 +0700

    change base size

--- components/molecules/product/productDetail.js
+++ components/molecules/product/productDetail.js
@@ -8,49 +8,49 @@ const ProductDetail = () => {
   return (
     <div
       className={cn(
-        isOpen && "space-y-3 lg:space-y-4",
+        isOpen && "space-y-4 lg:space-y-5",
         "w-full bg-[#F8F9FB] p-3 lg:p-4"
       )}
     >
-      <DropdownTrigger open={isOpen} onOpen={setIsOpen} className="text-xl lg:text-2xl">
+      <DropdownTrigger
+        open={isOpen}
+        onOpen={setIsOpen}
+        className="text-xl md:text-2xl"
+      >
         Details
       </DropdownTrigger>
 
       <DropdownWrapper isOpen={isOpen}>
-        <div className="grid gap-2.5 lg:w-2/3 lg:gap-3.5 ">
-          <div className="space-y-2">
-            <p className="font-oswald text-xs uppercase text-black">
-              about product
-            </p>
-            <p className="font-roboto text-xs text-[#3F3F3F]">
+        <div className="grid gap-3 lg:w-2/3 lg:gap-4 ">
+          <div className="space-y-2 text-xs md:text-sm">
+            <p className="font-oswald uppercase text-black">about product</p>
+            <p className="font-roboto text-[#3F3F3F]">
               Cool off this summer in the Mini Ruffle Smocked Tank Top from our
               very own LA Hearts. This tank features a smocked body, adjustable
               straps, scoop neckline, ruffled hems, and a cropped fit.
             </p>
           </div>
 
-          <div className="space-y-2">
-            <p className="font-oswald text-xs uppercase text-black">shipping</p>
-            <p className="font-roboto text-xs text-[#3F3F3F]">
+          <div className="space-y-2 text-xs md:text-sm">
+            <p className="font-oswald uppercase text-black">shipping</p>
+            <p className="font-roboto text-[#3F3F3F]">
               We offer Free Standard Shipping for all orders over $75 to the 50
               states and the District of Columbia. The minimum order value must
               be $75 before taxes, shipping and handling. Shipping fees are
               non-refundable.
             </p>
-            <p className="font-roboto text-xs text-[#3F3F3F]">
+            <p className="font-roboto text-[#3F3F3F]">
               Please allow up to 2 business days (excluding weekends, holidays,
               and sale days) to process your order.
             </p>
-            <p className="font-roboto text-xs text-[#3F3F3F]">
+            <p className="font-roboto text-[#3F3F3F]">
               Processing Time + Shipping Time = Delivery Time
             </p>
           </div>
 
-          <div className="space-y-2">
-            <p className="font-oswald text-xs uppercase text-black">
-              advantages
-            </p>
-            <ul className="grid list-inside list-disc gap-2 font-roboto text-xs text-[#3F3F3F]">
+          <div className="space-y-2 text-xs md:text-sm">
+            <p className="font-oswald uppercase text-black">advantages</p>
+            <ul className="grid list-inside list-disc gap-1.5 font-roboto text-[#3F3F3F]">
               <li>Smocked body</li>
               <li>Adjustable straps</li>
               <li>Scoop neckline</li>
