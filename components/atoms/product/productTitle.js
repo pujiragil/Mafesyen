1201752f45c0368bae6329969581b4043f7962d1
pujiragil <ragilsg164@gmail.com>
2023-08-21 23:51:48 +0700

    change base size

--- components/atoms/product/productTitle.js
+++ components/atoms/product/productTitle.js
@@ -1,6 +1,6 @@
 const ProductTitle = ({ children }) => {
   return (
-    <h2 className="font-oswald text-3xl font-medium text-black lg:text-4xl">
+    <h2 className="font-oswald text-3xl font-medium text-black md:text-4xl">
       {children}
     </h2>
   );
