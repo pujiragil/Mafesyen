44577c0eb88d6bebcf9113c5afc4b48544ba8920
pujiragil <ragilsg164@gmail.com>
2023-08-24 00:46:07 +0700

    fix width issue for swiper product

--- components/molecules/product/productSliderImage.js
+++ components/molecules/product/productSliderImage.js
@@ -7,30 +7,32 @@ import { ProductSliderPagination } from "@/components/molecules";
 
 const ProductSliderImage = ({ thumb, data }) => {
   return (
-    <Swiper
-      modules={[Thumbs]}
-      thumbs={{
-        swiper: thumb && !thumb.destroyed ? thumb : null,
-      }}
-      spaceBetween={16}
-      className="relative flex h-full w-full flex-col gap-3.5"
-    >
-      {data.map((image) => (
-        <SwiperSlide key={image.id}>
-          <Image
-            width={1920}
-            height={1280}
-            loading="lazy"
-            placeholder="blur"
-            blurDataURL={image.blurDataUrl}
-            alt={image.alt}
-            src={`/images/dress/${image.link}`}
-            className="aspect-square w-full object-cover object-top md:aspect-auto md:h-full lg:aspect-square"
-          />
-        </SwiperSlide>
-      ))}
-      <ProductSliderPagination />
-    </Swiper>
+    <div className="grid grid-cols-1 h-full">
+      <Swiper
+        modules={[Thumbs]}
+        thumbs={{
+          swiper: thumb && !thumb.destroyed ? thumb : null,
+        }}
+        spaceBetween={16}
+        className="relative flex h-full w-full flex-col gap-3.5"
+      >
+        {data.map((image) => (
+          <SwiperSlide key={image.id}>
+            <Image
+              width={1920}
+              height={1280}
+              loading="lazy"
+              placeholder="blur"
+              blurDataURL={image.blurDataUrl}
+              alt={image.alt}
+              src={`/images/dress/${image.link}`}
+              className="aspect-square w-full object-cover object-top md:aspect-auto md:h-full lg:aspect-square"
+            />
+          </SwiperSlide>
+        ))}
+        <ProductSliderPagination />
+      </Swiper>
+    </div>
   );
 };
 
--- components/molecules/product/productSliderThumb.js
+++ components/molecules/product/productSliderThumb.js
@@ -5,31 +5,33 @@ import { Thumbs } from "swiper/modules";
 
 const ProductSliderThumb = ({ onSlide, data }) => {
   return (
-    <Swiper
-      onSwiper={onSlide}
-      modules={[Thumbs]}
-      scrollbar={true}
-      direction="vertical"
-      slidesPerView="auto"
-      spaceBetween={14}
-      watchSlidesProgress={true}
-      className="thumbs-swiper hidden h-full w-full overflow-hidden lg:flex"
-    >
-      {data.map((image) => (
-        <SwiperSlide key={image.id}>
-          <Image
-            width={1920}
-            height={1280}
-            loading="lazy"
-            placeholder="blur"
-            blurDataURL={image.blurDataUrl}
-            src={`/images/dress/${image.link}`}
-            alt={image.alt}
-            className="h-24 w-full cursor-pointer object-cover object-center"
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="grid grid-cols-1">
+      <Swiper
+        onSwiper={onSlide}
+        modules={[Thumbs]}
+        scrollbar={true}
+        direction="vertical"
+        slidesPerView="auto"
+        spaceBetween={14}
+        watchSlidesProgress={true}
+        className="thumbs-swiper hidden h-full w-full overflow-hidden lg:flex"
+      >
+        {data.map((image) => (
+          <SwiperSlide key={image.id}>
+            <Image
+              width={1920}
+              height={1280}
+              loading="lazy"
+              placeholder="blur"
+              blurDataURL={image.blurDataUrl}
+              src={`/images/dress/${image.link}`}
+              alt={image.alt}
+              className="curso r-pointer h-24  w-full object-cover object-center"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
