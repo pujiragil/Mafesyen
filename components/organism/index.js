7d816337e8dc973f7f4bd99ce1f94e39e8583d61
pujiragil <ragilsg164@gmail.com>
2023-09-04 23:51:30 +0700

    created ProductCart component

--- components/organism/index.js
+++ components/organism/index.js
@@ -1,6 +1,7 @@
 export { default as BlogSection } from "./blog/blogSection";
+export { default as OfferCardSection } from "./card/offerCardSection";
 export { default as FilterTab } from "./filter";
 export { default as Footer } from "./footer";
-export { default as OfferCardSection } from "./card/offerCardSection";
+export { default as ProductCart } from "./product/productCart";
 export { default as ProductSlider } from "./product/productSlider";
 export { default as HeroSliderSection } from "./slider/heroSliderSection";
