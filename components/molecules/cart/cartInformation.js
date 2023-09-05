566b202b689211ac1a140b906b430a837617f672
pujiragil <ragilsg164@gmail.com>
2023-09-05 11:23:11 +0700

    added font size for different screen

--- components/molecules/cart/cartInformation.js
+++ components/molecules/cart/cartInformation.js
@@ -1,11 +1,13 @@
 const CartInformation = ({ title, color, size, price }) => {
   return (
     <div className="space-y-1">
-      <p className="font-oswald text-base font-normal text-black">{title}</p>
-      <p className="font-roboto text-xs text-[#C4C4C4]">
+      <p className="font-oswald text-lg font-normal text-black md:text-xl">
+        {title}
+      </p>
+      <p className="font-roboto text-xs text-[#C4C4C4] md:text-sm">
         {color}, {size}
       </p>
-      <p className="font-oswald text-sm font-normal uppercase text-black">
+      <p className="font-oswald text-sm font-normal uppercase text-black md:text-base">
         IDR {price}
       </p>
     </div>
