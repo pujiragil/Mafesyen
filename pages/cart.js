7ec147c6b8b4948f413cacb391bfdd98c796f06f
pujiragil <ragilsg164@gmail.com>
2023-09-05 11:24:41 +0700

    set max width for cart page

--- pages/cart.js
+++ pages/cart.js
@@ -4,17 +4,17 @@ import { ProductCart } from "@/components/organism";
 export default function Cart() {
   return (
     <BaseLayout title="Mafesyen - Cart">
-      <div className="container mx-auto">
+      <div className="container mx-auto max-w-sm sm:max-w-md">
         <div className="mx-3.5 space-y-6 py-3.5">
-          <h1 className="border-b border-[#C4C4C4] pb-2.5 font-oswald text-lg font-normal text-black">
+          <h1 className="border-b border-[#C4C4C4] pb-2.5 font-oswald text-xl font-normal text-black md:text-2xl">
             Shopping Cart
           </h1>
 
           <div className="grid grid-cols-1 gap-3">
-            <ProductCart image="/images/dress/floral-women-sundress-blue-1.webp"/>
-            <ProductCart image="/images/dress/floral-women-sundress-dark-1.webp"/>
-            <ProductCart image="/images/dress/floral-women-sundress-purple-1.webp"/>
-            <ProductCart image="/images/dress/floral-women-sundress-red-1.webp"/>
+            <ProductCart image="/images/dress/floral-women-sundress-blue-1.webp" />
+            <ProductCart image="/images/dress/floral-women-sundress-dark-1.webp" />
+            <ProductCart image="/images/dress/floral-women-sundress-purple-1.webp" />
+            <ProductCart image="/images/dress/floral-women-sundress-red-1.webp" />
           </div>
         </div>
       </div>
