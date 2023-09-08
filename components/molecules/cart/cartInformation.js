e7763930394a63e3ec90680e34344c9a459f2e12
pujiragil <ragilsg164@gmail.com>
2023-09-08 23:50:40 +0700

    change to compound component

--- components/molecules/cart/cartInformation.js
+++ components/molecules/cart/cartInformation.js
@@ -1,17 +1,53 @@
-const CartInformation = ({ title, color, size, price }) => {
+import { createContext, useContext } from "react";
+
+const CartInformationContext = createContext(null);
+
+const useCartInformationContext = () => useContext(CartInformationContext);
+
+const CartInformation = ({ title, type, price, data }) => {
   return (
-    <div className="space-y-1">
-      <p className="font-oswald text-lg font-normal text-black md:text-xl">
+    <CartInformationContext.Provider value={data}>
+      <div className="space-y-1">
         {title}
-      </p>
-      <p className="font-roboto text-xs text-[#C4C4C4] md:text-sm">
-        {color}, {size}
-      </p>
-      <p className="font-oswald text-sm font-normal uppercase text-black md:text-base">
-        IDR {price}
-      </p>
-    </div>
+        {type}
+        {price}
+      </div>
+    </CartInformationContext.Provider>
   );
 };
+
+const Title = () => {
+  const data = useCartInformationContext();
+
+  return (
+    <p className="font-oswald text-lg font-normal text-black md:text-xl">
+      {data.title}
+    </p>
+  );
+};
+
+const Type = () => {
+  const data = useCartInformationContext();
+
+  return (
+    <p className="font-roboto text-xs text-[#C4C4C4] md:text-sm">
+      {data.color}, {data.size}
+    </p>
+  );
+};
+
+const Price = () => {
+  const data = useCartInformationContext();
+
+  return (
+    <p className="font-oswald text-sm font-normal uppercase text-black md:text-base">
+      IDR {data.price}
+    </p>
+  );
+};
+
+CartInformation.Title = Title;
+CartInformation.Type = Type;
+CartInformation.Price = Price;
 
 export default CartInformation;
