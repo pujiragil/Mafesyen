62ab4931b4e0f73185e3a51cf6f80c1aa9d6d98b
pujiragil <ragilsg164@gmail.com>
2023-09-13 22:27:34 +0700

    created wishlist page

---
+++ pages/wishlist.js
@@ -0,0 +1,87 @@
+import BaseLayout from "@/components/Layout/baseLayout";
+import SectionLayout from "@/components/Layout/sectionLayout";
+import {
+  ProductCard,
+  ProductImage,
+  ProductInfo,
+  ProductPrice,
+} from "@/components/molecules";
+import { cn } from "@/utils/utils";
+
+import data from "@/utils/data.json";
+
+export default function Wishlist() {
+  return (
+    <BaseLayout title="Mafesyen - My Wishlist">
+      <SectionLayout>
+        <div className="mx-3.5 grid place-items-center py-3.5 md:gap-6">
+          <h3 className="hidden font-oswald text-2xl font-normal text-black md:block lg:text-3xl">
+            My Wishlist
+          </h3>
+          <div className="grid w-full max-w-md gap-4 sm:max-w-none md:grid-cols-[1fr_3fr] md:gap-2 lg:grid-cols-[1fr_4fr]">
+            <div className="flex h-10 w-full items-center justify-between bg-[#F0F1F2] px-2.5 md:hidden">
+              <p className="font-oswald text-xs font-medium uppercase text-black">
+                my wishlist
+              </p>
+
+              <svg
+                className={cn(
+                  "w-3 md:w-4",
+                  "cursor-pointer",
+                  "stroke-black",
+                  "rotate-180"
+                )}
+                viewBox="0 0 10 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M9 5L5 1L1 5" />
+              </svg>
+            </div>
+
+            <Sidebar />
+
+            <div className="grid gap-2.5">
+              <p className="font-oswald text-sm font-normal uppercase text-[#828282] md:hidden">
+                my wishlist
+              </p>
+
+              <ProductWishlist />
+            </div>
+          </div>
+        </div>
+      </SectionLayout>
+    </BaseLayout>
+  );
+}
+
+const ProductWishlist = () => {
+  const products = data.products;
+
+  return (
+    <div className="grid grid-cols-2 gap-3.5 sm:grid-cols-3 lg:grid-cols-4">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          data={product}
+          image={<ProductImage />}
+          info={<ProductInfo />}
+          price={<ProductPrice />}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  return (
+    <div className="hidden space-y-1.5 font-oswald font-normal text-[#828282] md:block">
+      <p>Account Dashboard</p>
+      <p>Account Information</p>
+      <p>Adress Book</p>
+      <p>My Orders</p>
+      <p className="text-black">My Wishlist</p>
+      <p>Newsletter Subscriptions</p>
+    </div>
+  );
+};
