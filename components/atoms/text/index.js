c948a2c91349605639d61e14be109a36013db198
pujiragil <ragilsg164@gmail.com>
2023-09-14 11:40:49 +0700

    added text size component

--- components/atoms/text/index.js
+++ components/atoms/text/index.js
@@ -12,6 +12,8 @@ const text = cva("font-light text-black", {
       "xs/sm/base": "text-xs md:text-sm lg:text-base",
       "xs/lg": "text-xs md:text-base lg:text-lg",
       "xs/2xl": "text-xs md:text-2xl",
+      "sm/lg": "text-sm md:text-lg",
+      "sm/xl": "text-sm md:text-xl",
       "sm/2xl": "text-sm md:text-2xl",
     },
     family: {
