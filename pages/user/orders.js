ffa33ddf2b02fa156805cb40f477d085c648979a
pujiragil <ragilsg164@gmail.com>
2023-09-26 23:39:28 +0700

    created user/orders page

---
+++ pages/user/orders.js
@@ -0,0 +1,9 @@
+import UserAccessLayout from "@/components/Layout/userAccessLayout";
+
+export default function Orders() {
+  return <UserAccessLayout title="My Orders">
+   <div>
+    
+   </div>
+  </UserAccessLayout>;
+}
