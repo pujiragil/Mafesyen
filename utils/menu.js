481d9b6a5f9f0bff31fee06e0b80f1120a921df1
pujiragil <ragilsg164@gmail.com>
2023-09-26 23:40:43 +0700

    added menu as dummy data

---
+++ utils/menu.js
@@ -0,0 +1,29 @@
+const userAccessMenu = [
+  {
+    id: "dashboard",
+    to: "/dashboard",
+    name: "Account Dashboard",
+  },
+  {
+    id: "information",
+    to: "/information",
+    name: "Account Information",
+  },
+  {
+    id: "address",
+    to: "/address-book",
+    name: "Adress Book",
+  },
+  {
+    id: "orders",
+    to: "/orders",
+    name: "My Orders",
+  },
+  {
+    id: "wishlist",
+    to: "/wishlist",
+    name: "My Wishlist",
+  },
+];
+
+export default userAccessMenu;
